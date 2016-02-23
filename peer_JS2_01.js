f5715cfa657538ac660e8ceaf404367968e812aa
Roman H <ipero@live.com>
2016-02-23 17:33:42 -0600

    Final work

---
+++ peer_JS2_01.js
@@ -0,0 +1,121 @@
+/*
+1. Start with the number 68 and set that equal to 'value'
+
+2. Create condition logic to check if the value is great or equal to 81
+2-1. If true, add 9 to 'value'
+2-2. If false, subtract 1 from 'value'
+
+3. Create a string that is set to 95, add it to 'value'
+
+4. Create an array, loop through 'value' using charAt, set array[i] to each value
+
+5. Remove the first and last values off the array
+
+6. Create a new variable. Loop backwards through the array and store each value into the new Variable, combining each of the values of that array (backwards remember!)
+
+7. parseInt both the 'value' and the new variable created in Step 6, ensure that both 'value' and the new variable are set to these new parsed values;
+
+******* SWITCH PROGRAMMERS *******
+
+8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
+
+9. If the new value of 'value' is greater than 3271, set 'value' equal to 59. If not, check to see if it is equal to 1069, if it is, set 'value' equal to 95.
+If neither of these are true, set the value to 11.
+
+10. Create a while loop that counts down from 6 and increments 'value' by 1.
+
+11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there
+is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
+
+12. Call the function.
+
+13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
+
+14. Your answer should be a String value that equals 5. Is that what you got?
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var value=68;
+if(value>=81){
+	value+=9;
+
+}else{
+	value-=1;
+}
+value+="95";
+var array=[];
+for(var i=0;i<value.length;i++){
+	array.push(value.charAt(i));
+}
+
+array.shift();
+array.pop();
+var newValue="";
+for(i=array.length-1;i>=0;i--){
+	newValue+=array[i];
+}
+value=parseInt(value);
+newValue=parseInt(newValue)
+console.log(value);
+console.log(newValue);
+
+// Switched People
+
+//8
+value += newValue;
+//9
+if (value > 3271) {
+	value = 59;
+} else if (value == 1069) {
+	value = 295;
+} else {
+	value = 11;
+}
+//10
+i = 6;
+while (i  > 0){
+	value++;
+	i--;
+}
+
+//11
+function someFunction(val) {
+	val = val.toString();
+	if (val.length > 1) {
+		val = val.substr(1);
+	}
+	return val;
+}
+
+value = someFunction(value);
+
+console.log(value);
